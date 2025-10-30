944d85d246ddae209223b320cfd1ffdee0a53a31
NJKITTENS555 <nicholaswiney@gmail.com>
2025-10-30 16:12:18 +0000

    666

--- npcgenorator.py
+++ npcgenorator.py
@@ -1,2 +1,15 @@
 import random 
-names = ["joe shmoe", "timmy", "timothy", "eggward", "Docter Fish Sticks", "See Squared", "Gwood", "Frederick Faz", "Chuckles the clown", "Kludzy the clown", ]
+names_list = ["joe schmoe", "doug", "timmy", "timothy", "Eggward", "Garry", "Gwood", "Frederick Faz", "Chuckles", "Kludzy", "Larry", "" ]
+for name in names_list :
+    
+
+    current_hp = random.randint(0, 100)
+    if current_hp is 0:
+        status = "dead"
+    else:
+        status = "alive"
+    
+    clutz = random.randint(1,2)
+    clumsy = (clutz == 2) 
+    speed = random.randint(0, 100)
+    print(f"Name: {name}, Status: {status}, Speed : {speed}, Clumsy : {clumsy}, health: {current_hp}, ")
